9be5d655fd96c970122d19edfde52afce3b9eb8d
tkalayci71 <114084637+tkalayci71@users.noreply.github.com>
2022-12-31 01:23:22 +0300

    v2.57

--- scripts/embedding_inspector.py
+++ scripts/embedding_inspector.py
@@ -1,7 +1,7 @@
 # Embedding Inspector extension for AUTOMATIC1111/stable-diffusion-webui
 #
 # https://github.com/tkalayci71/embedding-inspector
-# version 2.56 - 2022.12.30
+# version 2.57 - 2022.12.31
 #
 
 import gradio as gr
@@ -19,6 +19,16 @@ SEP_STR = '-'*80 # separator string
 ENABLE_GRAPH = True
 ENABLE_SHOW_CHECKSUM = False #slows down listing loaded embeddings
 REMOVE_ZEROED_VECTORS = True #optional
+
+EVAL_PRESETS = ['None','',
+    'Boost','=v*8',
+    'Digitize','=math.ceil(v*8)/8',
+    'Randomize','=v*random.random()',
+    'Sine','=v*math.sin(i/maxi*math.pi)',
+    'Comb','=v*((i%2)==0)',
+    'Crop_high','=v*(i<maxi//2)',
+    'Crop_low','=v*(i>=maxi//2)'
+    ]
 
 #-------------------------------------------------------------------------------
 
@@ -122,12 +132,12 @@ def get_embedding_info(text):
 def do_inspect(text):
 
     text = text.strip().lower()
-    if (text==''): return 'Need embedding name or embedding ID as #nnnnn'
+    if (text==''): return 'Need embedding name or embedding ID as #nnnnn', None
 
     # get the embedding info for first token in text
     emb_name, emb_id, emb_vec, loaded_emb = get_embedding_info(text)
     if (emb_name==None) or (emb_id==None) or (emb_vec==None):
-        return 'An error occurred'
+        return 'An error occurred', None
 
     results = []
 
@@ -192,9 +202,48 @@ def do_inspect(text):
 
         results.append(SEP_STR)
 
-    return '\n'.join(results) # return info string to results textbox
+    saved_graph = None
+
+    if ENABLE_GRAPH:
+        # save graph
+        #try:
+            from matplotlib import pyplot as plt
+
+            emb_vec = emb_vec.to(device='cpu',dtype=torch.float32).clone()
+
+            fig = plt.figure()
+            for u in range(emb_vec.shape[0]):
+                x = torch.arange(start=0,end=emb_vec[u].shape[0],step=1)
+                plt.plot(x.numpy(), emb_vec[u].numpy())
+
+            saved_graph = fig2img(fig)
+        #except:
+        #    saved_graph = None
+
+    return '\n'.join(results), saved_graph # return info string to results textbox and graph
 
 #-------------------------------------------------------------------------------
+
+def do_save_vector(text, fnam):
+
+    text = text.strip().lower()
+    if (text==''): return
+
+    # get the embedding info for first token in text
+    emb_name, emb_id, emb_vec, loaded_emb = get_embedding_info(text)
+    if (emb_name==None) or (emb_id==None) or (emb_vec==None):
+        return
+
+    emb_vec = emb_vec.to(device='cpu',dtype=torch.float32).clone()
+
+    torch.set_printoptions(threshold=10000,profile='full')
+    fnam = fnam.strip()
+    if fnam=='': fnam = 'emb_vec.txt'
+    with open(fnam, 'w') as f:
+        f.write(emb_name+'\n\n')
+        f.write(str(emb_vec))
+        f.close()
+    return
 
 def do_save(*args):
 
@@ -205,134 +254,155 @@ def do_save(*args):
     concat_mode = args[-4]
     eval_txt = args[-5].strip()
     combine_mode = args[-6]
+    batch_save_presets = args[-7]
     if save_name=='':return 'Filename is empty', None
 
     results = []
 
-    save_filename = 'embeddings/'+save_name+'.bin'
-    file_exists = os.path.exists(save_filename)
-    if (file_exists):
-        if not(enable_overwrite):
-            return('File already exists, overwrite not enabled, aborting save.', None)
-        else:
-            results.append('File already exists, overwrite is enabled')
+    preset_count = 1 #corresponds to 'None' preset, will use eval_txt
+    if batch_save_presets==True: preset_count = len(EVAL_PRESETS)//2
 
-    step_val = None
-    try:
-        step_val = int(step_text)
-    except:
-        step_val = None
-        if (step_text!=''): results.append('Step value is invalid, ignoring')
-
-    # calculate mixed embedding in tot_vec
-    vec_size = None
-    tot_vec = None
-    for k in range(MAX_NUM_MIX):
-        name= args[k].strip().lower()
-
-        mixval = args[k+MAX_NUM_MIX]
-        if (name=='') or (mixval==0): continue
-
-        emb_name, emb_id, emb_vec, loaded_emb = get_embedding_info(name)
-        mix_vec = emb_vec.to(device='cpu',dtype=torch.float32)
-
-        if vec_size==None:
-            vec_size = mix_vec.shape[1]
-        else:
-            if vec_size!=mix_vec.shape[1]:
-                results.append('! Vector size is not compatible, skipping '+emb_name+'('+str(emb_id)+')')
-                continue
-
-        if not(concat_mode):
-            if tot_vec==None:
-                tot_vec = torch.zeros(vec_size).unsqueeze(0)
-
-            if mix_vec.shape[0]!=tot_vec.shape[0]:
-                padding = torch.zeros(abs(tot_vec.shape[0]-mix_vec.shape[0]),vec_size)
-                if mix_vec.shape[0]<tot_vec.shape[0]:
-                    mix_vec = torch.cat([mix_vec, padding])
-                else:
-                    tot_vec = torch.cat([tot_vec, padding])
-
-            tot_vec+= mix_vec * mixval
-            results.append('+ '+emb_name+'('+str(emb_id)+')'+' x '+str(mixval))
-        else:
-            if tot_vec==None:
-                tot_vec = mix_vec*mixval
-            else:
-                tot_vec = torch.cat([tot_vec,mix_vec*mixval])
-            results.append('> '+emb_name+'('+str(emb_id)+')'+' x '+str(mixval))
-
+    anything_saved = False
     saved_graph = None
-    # save the mixed embedding
-    if (tot_vec==None):
-        results.append('No embeddings were mixed, nothing to save')
-    else:
-        #eval feautre
-        if eval_txt!='':
-            vec = tot_vec.clone()
-            try:
-                maxn = vec.shape[0]
-                maxi = vec.shape[1]
-                for n in range(maxn):
-                    if eval_txt.startswith('='):
-                        #item-wise eval
-                        for i in range(maxi):
-                            v = vec[n,i]
-                            ve = eval(eval_txt[1:]) #strip "="
-                            vec[n,i] = ve
+
+    for preset_no in range(preset_count):
+
+        #------------- start batch loop, old behaviour if preset_count==1
+
+        preset_name = ''
+        if (preset_no>0):
+            preset_name = '_'+EVAL_PRESETS[preset_no*2]
+            eval_txt = EVAL_PRESETS[preset_no*2+1]
+
+        save_filename = 'embeddings/'+save_name+preset_name+'.bin'
+        file_exists = os.path.exists(save_filename)
+        if (file_exists):
+            if not(enable_overwrite):
+                return('File already exists ('+save_filename+') overwrite not enabled, aborting.', None)
+            else:
+                results.append('File already exists, overwrite is enabled')
+
+        step_val = None
+        try:
+            step_val = int(step_text)
+        except:
+            step_val = None
+            if (step_text!=''): results.append('Step value is invalid, ignoring')
+
+        # calculate mixed embedding in tot_vec
+        vec_size = None
+        tot_vec = None
+        for k in range(MAX_NUM_MIX):
+            name= args[k].strip().lower()
+
+            mixval = args[k+MAX_NUM_MIX]
+            if (name=='') or (mixval==0): continue
+
+            emb_name, emb_id, emb_vec, loaded_emb = get_embedding_info(name)
+            mix_vec = emb_vec.to(device='cpu',dtype=torch.float32)
+
+            if vec_size==None:
+                vec_size = mix_vec.shape[1]
+            else:
+                if vec_size!=mix_vec.shape[1]:
+                    results.append('! Vector size is not compatible, skipping '+emb_name+'('+str(emb_id)+')')
+                    continue
+
+            if not(concat_mode):
+                if tot_vec==None:
+                    tot_vec = torch.zeros(vec_size).unsqueeze(0)
+
+                if mix_vec.shape[0]!=tot_vec.shape[0]:
+                    padding = torch.zeros(abs(tot_vec.shape[0]-mix_vec.shape[0]),vec_size)
+                    if mix_vec.shape[0]<tot_vec.shape[0]:
+                        mix_vec = torch.cat([mix_vec, padding])
                     else:
-                        #tensor-wise eval
-                        v = vec[n]
-                        ve = eval(eval_txt)
-                        vec[n] = ve
-                tot_vec = vec
-                results.append('Applied eval: "'+eval_txt+'"')
-            except Exception as e:
-                results.append('Error evaluating: "'+eval_txt+'" - '+str(e))
+                        tot_vec = torch.cat([tot_vec, padding])
 
-        if (combine_mode and (tot_vec.shape[0]>1)):
-            results.append('combining '+str(tot_vec.shape[0])+' vectors as 1-vector')
-            tot_vec = torch.sum(tot_vec,dim=0,keepdim=True)
+                tot_vec+= mix_vec * mixval
+                results.append('+ '+emb_name+'('+str(emb_id)+')'+' x '+str(mixval))
+            else:
+                if tot_vec==None:
+                    tot_vec = mix_vec*mixval
+                else:
+                    tot_vec = torch.cat([tot_vec,mix_vec*mixval])
+                results.append('> '+emb_name+'('+str(emb_id)+')'+' x '+str(mixval))
 
-
-        if REMOVE_ZEROED_VECTORS:
-            old_count = tot_vec.shape[0]
-            tot_vec = tot_vec[torch.count_nonzero(tot_vec,dim=1)>0]
-            new_count = tot_vec.shape[0]
-            if (old_count!=new_count): results.append('Removed '+str(old_count-new_count)+' zeroed vectors, remaining vectors: '+str(new_count))
-
-        if tot_vec.shape[0]>0:
-            new_emb = Embedding(tot_vec, save_name)
-            if (step_val!=None):
-                new_emb.step = step_val
-                results.append('Setting step value to '+str(step_val))
-
-            try:
-                new_emb.save(save_filename)
-                results.append('Saved "'+save_filename+'"')
-
-            except:
-                results.append('Error saving "'+save_filename+'" (filename might be invalid)')
-
-            results.append('Reloading all embeddings')
-            sd_hijack.model_hijack.embedding_db.dir_mtime=0
-            sd_hijack.model_hijack.embedding_db.load_textual_inversion_embeddings()
-
-
-            if ENABLE_GRAPH:
-                # save graph
+        # save the mixed embedding
+        if (tot_vec==None):
+            results.append('No embeddings were mixed, nothing to save')
+        else:
+            #eval feautre
+            if eval_txt!='':
+                vec = tot_vec.clone()
                 try:
-                    from matplotlib import pyplot as plt
+                    maxn = vec.shape[0]
+                    maxi = vec.shape[1]
+                    for n in range(maxn):
+                        if eval_txt.startswith('='):
+                            #item-wise eval
+                            for i in range(maxi):
+                                v = vec[n,i]
+                                ve = eval(eval_txt[1:]) #strip "="
+                                vec[n,i] = ve
+                        else:
+                            #tensor-wise eval
+                            v = vec[n]
+                            ve = eval(eval_txt)
+                            vec[n] = ve
+                    tot_vec = vec
+                    results.append('Applied eval: "'+eval_txt+'"')
+                except Exception as e:
+                    results.append('Error evaluating: "'+eval_txt+'" - '+str(e))
 
-                    fig = plt.figure()
-                    for u in range(tot_vec.shape[0]):
-                        x = torch.arange(start=0,end=tot_vec[u].shape[0],step=1)
-                        plt.plot(x.numpy(), tot_vec[u].numpy())
+            if (combine_mode and (tot_vec.shape[0]>1)):
+                results.append('combining '+str(tot_vec.shape[0])+' vectors as 1-vector')
+                tot_vec = torch.sum(tot_vec,dim=0,keepdim=True)
 
-                    saved_graph = fig2img(fig)
+
+            if REMOVE_ZEROED_VECTORS:
+                old_count = tot_vec.shape[0]
+                tot_vec = tot_vec[torch.count_nonzero(tot_vec,dim=1)>0]
+                new_count = tot_vec.shape[0]
+                if (old_count!=new_count): results.append('Removed '+str(old_count-new_count)+' zeroed vectors, remaining vectors: '+str(new_count))
+
+            if tot_vec.shape[0]>0:
+                new_emb = Embedding(tot_vec, save_name)
+                if (step_val!=None):
+                    new_emb.step = step_val
+                    results.append('Setting step value to '+str(step_val))
+
+                try:
+                    new_emb.save(save_filename)
+                    results.append('Saved "'+save_filename+'"')
+                    anything_saved = True
+
                 except:
-                    saved_graph = None
+                    results.append('Error saving "'+save_filename+'" (filename might be invalid)')
+
+            #------------- end batch loop
+
+
+    if anything_saved==True:
+
+        results.append('Reloading all embeddings')
+        sd_hijack.model_hijack.embedding_db.dir_mtime=0
+        sd_hijack.model_hijack.embedding_db.load_textual_inversion_embeddings()
+
+
+        if ENABLE_GRAPH:
+            # save graph (for last saved embedding in tot_vec)
+            try:
+                from matplotlib import pyplot as plt
+
+                fig = plt.figure()
+                for u in range(tot_vec.shape[0]):
+                    x = torch.arange(start=0,end=tot_vec[u].shape[0],step=1)
+                    plt.plot(x.numpy(), tot_vec[u].numpy())
+
+                saved_graph = fig2img(fig)
+            except:
+                saved_graph = None
 
     return '\n'.join(results), saved_graph  # return info string to log textbox and saved_graph
 
@@ -421,7 +491,22 @@ def do_reset(*args):
 
 #-------------------------------------------------------------------------------
 
+def do_eval_preset(*args):
+
+    preset_name = args[0]
+
+    result = ''
+    for e in range(len(EVAL_PRESETS)//2):
+        if preset_name == EVAL_PRESETS[e*2]:
+            result = EVAL_PRESETS[e*2+1]
+            break
+
+    return result
+
+#-------------------------------------------------------------------------------
+
 def add_tab():
+
     with gr.Blocks(analytics_enabled=False) as ui:
         with gr.Tabs():
             with gr.Row():
@@ -432,6 +517,13 @@ def add_tab():
                         inspect_button = gr.Button(value="Inspect", variant="primary")
                         listloaded_button = gr.Button(value="List loaded embeddings")
                     inspect_result = gr.Textbox(label="Results", lines=15)
+
+                    with gr.Row():
+                        with gr.Column():
+                            inspect_graph = gr.Image(visible=ENABLE_GRAPH)
+                        with gr.Column():
+                            save_vector_name = gr.Textbox(label="Filename",lines=1,placeholder='Enter file name to save (default=emb_vec.txt)',value="emb_vec.txt")
+                            save_vector_button = gr.Button(value="Save vector to text file")
 
                     with gr.Column(variant='panel'):
                         mini_input = gr.Textbox(label="Mini tokenizer", lines=1, placeholder="Enter a short prompt (loaded embeddings or modifiers are not supported)")
@@ -458,12 +550,19 @@ def add_tab():
                             with gr.Column():
                                 concat_mode = gr.Checkbox(value=False,label="Concat mode")
                                 combine_mode =  gr.Checkbox(value=False,label="combine as 1-vector")
-                            eval_box =  gr.Textbox(label="Eval",lines=2,placeholder='=v*2')
-                            step_box = gr.Textbox(label="Step",lines=1,placeholder='only for training')
+                                step_box = gr.Textbox(label="Step",lines=1,placeholder='only for training')
+
+                            with gr.Column():
+                                preset_names = []
+                                for i in range(len(EVAL_PRESETS)//2):
+                                    preset_names.append(EVAL_PRESETS[i*2])
+                                presets_dropdown = gr.Dropdown(label="Eval Preset",choices=preset_names)
+                                eval_box =  gr.Textbox(label="Eval",lines=2,placeholder='')
 
                     with gr.Row():
                         save_name = gr.Textbox(label="Filename",lines=1,placeholder='Enter file name to save')
                         save_button = gr.Button(value="Save mixed", variant="primary")
+                        batch_presets = gr.Checkbox(value=False,label="Save for ALL presets")
                         enable_overwrite = gr.Checkbox(value=False,label="Enable overwrite")
 
                     with gr.Row():
@@ -471,12 +570,16 @@ def add_tab():
                         save_graph = gr.Image()
 
             listloaded_button.click(fn=do_listloaded, outputs=inspect_result)
-            inspect_button.click(fn=do_inspect,inputs=[text_input],outputs=[inspect_result])
-            save_button.click(fn=do_save, inputs=mix_inputs+mix_sliders+[combine_mode, eval_box, concat_mode,save_name,enable_overwrite,step_box],outputs=[save_result, save_graph])
+            inspect_button.click(fn=do_inspect,inputs=[text_input],outputs=[inspect_result,inspect_graph])
+            save_button.click(fn=do_save, inputs=mix_inputs+mix_sliders+[batch_presets, combine_mode, eval_box, concat_mode,save_name,enable_overwrite,step_box],outputs=[save_result, save_graph])
 
             mini_tokenize.click(fn=do_minitokenize,inputs=mix_inputs+[combine_mode, concat_mode, mini_sendtomix, mini_input], outputs=mix_inputs+[concat_mode,combine_mode, mini_result])
 
             reset_button.click(fn=do_reset,outputs=mix_inputs+mix_sliders)
+
+            presets_dropdown.change(do_eval_preset,inputs=presets_dropdown,outputs=eval_box)
+
+            save_vector_button.click(fn=do_save_vector,inputs = [text_input, save_vector_name])
 
     return [(ui, "Embedding Inspector", "inspector")]
 
